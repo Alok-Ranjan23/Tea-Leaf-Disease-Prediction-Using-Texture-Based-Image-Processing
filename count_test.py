ee5a7f666da9f632f5961f18cdb48d1533f44f7a
Alok-Ranjan23 <40749307+Alok-Ranjan23@users.noreply.github.com>
2019-01-28 11:25:59 +0530

    Add files via upload

---
+++ count_test.py
@@ -0,0 +1,23 @@
+"""
+Below segment of code helps in finding initial count of tea leaf images in a particular folder('test') and helps in to keep track 
+of augmented images.
+"""
+
+import os
+
+
+# To count number of files in a folder
+def images():
+	for dirpath,dirname,file in os.walk('/home/ln-2/Desktop/Project/disease-final/test'):
+		print(len(file))
+		return(file)
+	"""
+	for file in glob.glob('*.jpg'):
+		c += 1
+		
+	return(c)
+	"""
+	
+	
+
+
